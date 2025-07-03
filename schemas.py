90ac583e3744394c306a53a112caa46787042955
Rahul-3-6-9 <159149712+Rahul-3-6-9@users.noreply.github.com>
2025-07-03 18:53:38 +0530

    Add files via upload

---
+++ schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class EquipmentRequest(BaseModel):
+    equipmentType: str = "EV Charger"
+    modelNo: str = "Terra 54"
+    manufacturer: Optional[str] = "ABB"
+    voltageRating: Optional[str] = "480"
+
+class EquipmentRequest1(BaseModel):
+    equipmentType: str = "EV Charger"
+
+class EquipmentRequest2(BaseModel):
+    equipmentType: str = "EV Charger"
+    manufacturer: str = "ABB"
+    types: list[str] = ["Level 2", "Level 3"]
+
